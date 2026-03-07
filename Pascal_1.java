3066deab34dc88db47aa84c370ec09d596a3c83d
Saket Hegde <sakethegde555@gmail.com>
2026-03-07 10:34:09 +0530

    programs added

---
+++ Pascal_1.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Pascal_1 {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> list = new ArrayList<>();
+        for(int i=0;i<numRows;i++)
+        {
+            List<Integer> m = new ArrayList<>();
+            m.add(1);
+            for(int j=1;j<i;j++)
+            {
+                m.add(list.get(i-1).get(j-1)+list.get(i-1).get(j));
+            }
+            if(i>0)  m.add(1);
+            list.add(m);
+        }
+        
+        return list;
+    }
+} {
+    
+}
