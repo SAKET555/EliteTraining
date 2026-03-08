4b767b7d13a2e819764650b483ac90ad4438f0c8
Saket Hegde <sakethegde555@gmail.com>
2026-03-08 12:13:18 +0530

    programs added

---
+++ LinkedListCycleHamilton.java
@@ -0,0 +1,20 @@
+public class LinkedListCycleHamilton {
+    public ListNode deleteDuplicates(ListNode head) {
+        ArrayList<Integer> a = new ArrayList<>();
+        ListNode curr = head;
+        while(curr!=null)
+        {
+            a.add(curr.val);
+            curr = curr.next;
+        }
+        while(curr!=null)
+        {
+            int l = curr.next.val;
+            if(curr.val == l)
+            {
+
+            }
+            curr = curr.next;
+        }
+    }
+}
