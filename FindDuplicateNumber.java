7541d0cb6273ef0c5b0efbe0397b04b60ff50871
SAKET-HEGDE <sakethegde555@gmail.com>
2026-03-09 21:13:58 +0530

    program added

---
+++ FindDuplicateNumber.java
@@ -0,0 +1,16 @@
+class FindDuplicateNumber {
+    public int findDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        HashSet<Integer> abc = new HashSet<>();
+        int repeatNumber = 0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(abc.contains(nums[i]))
+            {
+                repeatNumber = nums[i];
+            }
+            abc.add(nums[i]);
+        }
+        return repeatNumber;
+    }
+}
